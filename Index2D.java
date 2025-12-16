9e51dc25f4471c2a2411d775a033b9237b8a4543
dvirk <dvirkerenzvi@gmail.com>
2025-12-17 00:40:47 +0200

    1

--- Index2D.java
+++ Index2D.java
@@ -1,41 +1,55 @@
 package assignments.Ex2;
 
 public class Index2D implements Pixel2D {
+
+    private int _x;
+    private int _y;
+
     public Index2D(int w, int h) {
-        ;
+        _x=w;
+        _y=h;
     }
     public Index2D(Pixel2D other) {
-        ;
+         this._x = other.getX();
+         this._y = other.getY();
     }
     @Override
     public int getX() {
-
-        return 0;
+        return _x;
     }
 
     @Override
     public int getY() {
-
-        return 0;
+        return _y;
     }
 
     @Override
     public double distance2D(Pixel2D p2) {
-
-        return 0;
+        double dx = this.getX() - p2.getX();
+        double dy = this.getY() - p2.getY();
+        double t = (dx*dx+dy*dy);
+        return Math.sqrt(t);
     }
 
     @Override
     public String toString() {
         String ans = null;
-
+        ans = _x+","+_y;
         return ans;
     }
 
     @Override
     public boolean equals(Object p) {
         boolean ans = true;
-
+        if (p==null || !(p instanceof Index2D)){
+            ans = false;
+        }
+        else{
+            Index2D p2 = (Index2D)p;
+            if(p2.getX()==this._x && p2.getY()==this._y){
+                ans = true;
+            }
+        }
         return ans;
     }
 }
